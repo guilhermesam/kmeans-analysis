6f54ea25e28b2a80c540710d4c0020f8495d839a
Lucas Alexandre Fell <47724710+fell-lucas@users.noreply.github.com>
2021-05-04 16:20:36 -0300

    aproximação

--- tests/utils.py
+++ tests/utils.py
@@ -1,2 +1,33 @@
+from scipy.special import factorial
+
 def file_length(file):
   return file.shape[0]
+
+def funcao_fatorial(n, cpu):
+	'''
+	Aproximação fatorial
+	:param n: tamanho da instância
+	:param cpu: fator de conversão para tempo de CPU
+	:return: aproximação
+	'''
+	return (factorial(n) * cpu)
+
+
+def funcao_quadratica(n, cpu):
+	'''
+	Aproximação quadrática
+	:param n: tamanho da instância
+	:param cpu: fator de conversão para tempo de CPU
+	:return: aproximação
+	'''
+	return (n * n * cpu)
+
+
+def funcao_linear(n, cpu):
+	'''
+	Aproximação linear
+	:param n: tamanho da instância
+	:param cpu: fator de conversão para tempo de CPU
+	:return: aproximação
+	'''
+	return (n * cpu)
